eee13fed1dd5e5048aacd09899bf52336a87e048
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-25 15:35:31 -0500

    confetti added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,89 @@
-import React, {useState} from "react"
-import Die from "./components/Die"
+import React from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
+import useWindowSize from './useWindowSize';
+import Confetti from 'react-confetti'
 
 export default function App() {
 
-  const [gameBoard, setGameBoard] = useState(allNewDice())
+  const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+  const { width, height } = useWindowSize()
 
-  console.log(gameBoard);
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log("You won!")
+    }
+  }, [dice])
+  /**
+   * Challenge: Tie off loose ends!
+   * 1. If tenzies is true, Change the button text to "New Game"
+   * 2. If tenzies is true, use the "react-confetti" package to
+   *    render the <Confetti /> component 🎉
+   * 
+   *    Hint: don't worry about the `height` and `width` props
+   *    it mentions in the documentation.
+   */
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
   function allNewDice() {
-    ///random number between 1 and 6
     const newDice = []
-
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.floor(Math.random() * 6) + 1)
+      newDice.push(generateNewDie())
     }
-
     return newDice
   }
 
-  const dices = gameBoard.map((die, index) => <Die key={index} value={die} />)
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ?
+        die :
+        generateNewDie()
+    }))
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    }))
+  }
+
+  const diceElements = dice.map(die => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ))
 
   return (
     <main>
+      {/* Render Confetti component if `tenzies` is true*/}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
-        {dices}
+        {diceElements}
       </div>
+      <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+      {tenzies && <Confetti
+        width={width}
+        height={height}
+      />}
     </main>
   )
 }
--- src/components/Die.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Die(props) {
-  return (
-    <div className="die-face">
-      <h2 className="die-num">{props.value}</h2>
-    </div>
-  )
-}
